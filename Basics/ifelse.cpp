c032b1c27b37781d1c889f61cf84beed5974c303
atharva-923 <atharvaap9@gmail.com>
2026-04-30 11:48:39 +0530

    save

--- Basics/ifelse.cpp
+++ Basics/ifelse.cpp
@@ -7,8 +7,14 @@ int main(){
     if(age>=18){
         cout<<"You are an adult.";
     }
+    else if(age>10 && age<=17) {
+        cout<<"You are an teenager.";
+    }
+    else if(age>=1 && age<=10){
+        cout<<"You are an kid.";
+    }
     else{
-        cout<<"You are not an adult.";
+        cout<<"Invalid age.";
     }
     return 0;
 }
