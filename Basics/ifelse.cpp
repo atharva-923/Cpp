a6bf92e22961753d052b895f9be2646a72d9a1d4
atharva-923 <atharvaap9@gmail.com>
2026-05-04 20:47:38 +0530

    Switch

--- Basics/ifelse.cpp
+++ Basics/ifelse.cpp
@@ -18,3 +18,24 @@ int main(){
     }
     return 0;
 }
+//------------------------------Question--------------------------------------------
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int age ;
+//     cout<<"Enter age:";
+//     cin>>age;
+//     if(age<18){
+//         cout<<"not eligible for job";
+//     }
+//     else if(age<=54){//if age is greater than or equal to 18 and less than or equal to 54 then it will print eligible for job.
+//         cout<<"Eligible for job";
+//     }
+//     else if(age<=57){//if age is greater than 54 and less than or equal to 57 then it will print eligible for job but retirement soon.
+//         cout<<"Eligible for job, but retirement soon";
+//     }
+//     else{//if age is greater than 57 then it will print retirement.
+//         cout<<"Retirement";
+//     }        
+//     return 0;
+// }
