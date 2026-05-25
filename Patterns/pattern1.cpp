330b027ce2d5117ab1bdcef7c5fdd8f0b95936d3
atharva-923 <atharvaap9@gmail.com>
2026-05-25 13:46:42 +0530

    Patterns

---
+++ Patterns/pattern1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print1(int n){
+    for(int i = 0; i < n;i++){//outer loop for rows
+        for(int j = 0; j < n;j++){//inner loop for columns
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+// The function print1 takes an integer n as input and prints a square pattern of asteris. The outer loop runs n times, representing the number of rows. For each iteration of the outer loop, the inner loop also runs n times, printing an asterisk followed by a space for each column in that row. After the inner loop completes for a row, a newline character is printed to move to the next line. As a result, the function prints an n x n grid of asterisks.
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+
+    return 0;
+}
+// OUTPUT:-
+// 4
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * * 
---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print2(int n){
+    for(int i = 0 ; i<n ; i++){
+        for(int j = 0; j<=i ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+//same here the outer loop runs n times, representing the number of rows. For each iteration of the outer loop, the inner loop runs i+1 times, where i is the current row index (starting from 0). This means that for the first row (i=0), it prints 1 asterisk; for the second row (i=1), it prints 2 asterisks; and so on, until the nth row where it prints n asterisks. After the inner loop completes for each row, a newline character is printed to move to the next line. As a result, the function prints a right-angled triangle pattern of asteris with n rows.
+int main(){
+    int n;
+    cin>>n;
+    print2(n);
+
+    return 0;
+}
+// OUTPUT:-
+// 4
+// *
+// **
+// ***
+// ****
