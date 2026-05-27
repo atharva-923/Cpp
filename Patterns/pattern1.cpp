2c43ae1cff836d2aa178ebb16e4e65457401109c
atharva-923 <atharvaap9@gmail.com>
2026-05-27 13:09:04 +0530

    Patterns

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 void print1(int n){
-    for(int i = 0; i < n;i++){//outer loop for rows
-        for(int j = 0; j < n;j++){//inner loop for columns
+    for(int i = 0 ; i < n ; i++){//outer loop for rows
+        for(int j = 0 ; j < n ; j++){//inner loop for columns
             cout<<"* ";
         }
         cout<<endl;
--- Patterns/pattern2.cpp
+++ Patterns/pattern2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void print2(int n){
     for(int i = 0 ; i<n ; i++){
-        for(int j = 0; j<=i ; j++){
+        for(int j = 0 ; j<=i ; j++){
             cout<<"*";
         }
         cout<<endl;
