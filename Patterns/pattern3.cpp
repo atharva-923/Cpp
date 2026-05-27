2b5ee77e4c1c660b5c631ce6bfd826b44920bd12
atharva-923 <atharvaap9@gmail.com>
2026-05-27 13:33:51 +0530

    Patterns

--- Patterns/pattern3.cpp
+++ Patterns/pattern3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void print3(int n){
     for(int i = 1 ; i <= n ; i++){
-        for(int j= 1 ; j <= i ; j++){
+        for(int j = 1 ; j <= i ; j++){
             cout<< j <<" ";
         }
         cout<<endl;
