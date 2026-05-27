e3bc4ca002386c53a79fecba321947ae14931756
atharva-923 <atharvaap9@gmail.com>
2026-05-27 13:23:57 +0530

    Patterns

--- Patterns/pattern5.cpp
+++ Patterns/pattern5.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 void print5(int n){
-    for(int i = 0 ; i <= n ; i++){
+    for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < n-i ; j++){
             cout<<"*";
         }
