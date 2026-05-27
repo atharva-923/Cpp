6c79d98b43dd64437282301c435ff7a0c8326957
atharva-923 <atharvaap9@gmail.com>
2026-05-27 13:16:39 +0530

    Patterns

--- Patterns/pattern2.cpp
+++ Patterns/pattern2.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 void print2(int n){
-    for(int i = 0 ; i<n ; i++){
-        for(int j = 0 ; j<=i ; j++){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j <= i ; j++){
             cout<<"*";
         }
         cout<<endl;
